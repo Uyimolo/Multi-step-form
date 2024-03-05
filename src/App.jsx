e45cba232ce65ac6b844a8935d44062d255e795a
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-05 23:33:33 +0100

    stepCount logic added, stepCount state is now been updated properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import StepSelector from './components/StepSelector'
 import StepsContainer from './components/StepsContainer'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [stepCount, setStepCount] = useState(1)
+
+  useEffect(() => {
+    console.log(stepCount) //testing if state is been updated
+  },[stepCount])
 
   return (
      <div className="app">
-      <StepSelector />
-      <StepsContainer />
+      <StepSelector setStepCount={setStepCount} />
+      <StepsContainer stepCount={stepCount} setStepCount={setStepCount}  />
      </div>
   )
 }
